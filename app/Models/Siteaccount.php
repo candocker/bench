fd1031a4a6fac7ba0f025dd89099cf5886e3b0c6
acanhost <acanstudio@canliang.wang>
2021-08-18 17:53:30 +0800

    devleop bench

---
+++ app/Models/Siteaccount.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace ModuleBench\Models;
+
+class Siteaccount extends AbstractModel
+{
+    protected $table = 'siteaccount';
+    //protected $guarded = ['id'];
+
+}
---
+++ app/Models/Toolsort.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace ModuleBench\Models;
+
+class Toolsort extends AbstractModel
+{
+    protected $table = 'toolsort';
+    //protected $guarded = ['id'];
+
+}
